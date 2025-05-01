5dda77df083263c07a3065b6c1b56cb8d723dd04
Kierby Pepito <kierby1212@gmail.com>
2025-05-01 09:06:16 +0800

    Delete KierbyPortfolio directory

--- KierbyPortfolio/Models/PortfolioItem.cs
+++
@@ -1,9 +0,0 @@
-namespace KierbyPortfolio.Models
-{
-    public class PortfolioItem
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Link { get; set; }
-    }
-}
